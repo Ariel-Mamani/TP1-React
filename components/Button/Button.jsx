9b0597d3c9443aebc7c991c8e39378e3935bd712
DiegoMarchandon <diegomarc2020@gmail.com>
2025-04-18 20:00:18 -0300

    principio DRY para lógica de Button

--- components/Button/Button.jsx
+++ components/Button/Button.jsx
@@ -3,13 +3,9 @@ import React from "react";
 
 function Button({ icon: Icon, title, action, id, peliculas, setPeliculas }) {
     const handleClick = () => {
-        if(peliculas === undefined ){
-            console.log("peliculas es undefined");
-        }
 
-        /* if(action !== undefined){
+        if(action !== undefined){
             const peliculasActualizadas = Object.values(peliculas).map(pelicula => {
-                console.log(pelicula.id);
                 if(pelicula && pelicula.id === id){
                     if(action === "eliminar"){
                         pelicula.visto = null;
@@ -18,32 +14,13 @@ function Button({ icon: Icon, title, action, id, peliculas, setPeliculas }) {
                     }else{ // action == "vista"
                         pelicula.visto = true;
                     }
-                    return pelicula;
                 }
-            });
-            setPeliculas(peliculasActualizadas);
-            localStorage.setItem("peliculas", JSON.stringify(peliculasActualizadas));
-        } */
-
-        if (action === "eliminar") {
-            const peliculasActualizadas = peliculas.filter(pelicula => pelicula.id !== id);
-            setPeliculas(peliculasActualizadas);
-            localStorage.setItem("peliculas", JSON.stringify(peliculasActualizadas));
-        } else if (action === "porVer") {
-            const peliculasActualizadas = peliculas.map(pelicula => {
-                if (pelicula.id === id) pelicula.visto = false;
-                return pelicula;
-            });
-            setPeliculas(peliculasActualizadas);
-            localStorage.setItem("peliculas", JSON.stringify(peliculasActualizadas));
-        } else if (action === "vista") {
-            const peliculasActualizadas = peliculas.map(pelicula => {
-                if (pelicula.id === id) pelicula.visto = true;
                 return pelicula;
             });
             setPeliculas(peliculasActualizadas);
             localStorage.setItem("peliculas", JSON.stringify(peliculasActualizadas));
         }
+
     };
 
     return (
