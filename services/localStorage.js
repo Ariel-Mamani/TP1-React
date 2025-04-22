6962d34b0bc9ddad9f843cfd656861afea13deda
DiegoMarchandon <diegomarc2020@gmail.com>
2025-04-22 19:37:37 -0300

    comentando localStorage

--- services/localStorage.js
+++ services/localStorage.js
@@ -115,10 +115,10 @@ const peliculas = {
 }
 
 // verificamos si ya hay datos en localStorage
-if (!localStorage.getItem("peliculas")) {
+/* if (!localStorage.getItem("peliculas")) {
   // Si no hay nada, cargamos las películas por defecto
   localStorage.setItem("peliculas", JSON.stringify(peliculas)); 
-}
+} */
 
 
 // localStorage.setItem("peliculas", JSON.stringify(peliculas));
